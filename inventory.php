808a47b08ce745309b36ac741954cdca4ef9407b
Arjen van Bochoven <arjen@bochoven.net>
2013-11-16 23:39:52 +0100

    Moved inventory controller to module

---
+++ inventory.php
@@ -0,0 +1,87 @@
+<?php
+class inventory extends Controller
+{
+    // Require authentication
+    function __construct()
+    {
+        if( ! isset($_SESSION['user']))
+        {
+            redirect('auth/login');
+        }
+    } 
+
+    function index() {
+        
+        $data['page'] = 'inventory';
+
+        $obj = new View();
+        $obj->view('inventory/inventory_list', $data);
+    
+    }
+
+
+    function detail($serial) {
+
+        $inventoryitemobj = new InventoryItem();
+        $data['inventory_items'] = $inventoryitemobj->retrieve_many(
+                                        'serial=?', array($serial));
+        $data['page'] = 'inventory';
+    	$data['serial_number'] = $serial;
+
+        $obj = new View();
+        $obj->view('inventory/inventory_detail', $data);
+    }
+
+    function items($name='', $version='') {
+        if ($name)
+        {
+            $name = rawurldecode($name);
+            $inventory_item_obj = new InventoryItem();
+            $data['name'] = $name;
+            if ($version)
+            {
+                $version = rawurldecode($version);
+                $items = $inventory_item_obj->retrieve_many(
+                    'name = ? AND version = ?', array($name, $version));
+            } else {
+                $items = $inventory_item_obj->retrieve_many(
+                    'name = ?', array($name));
+            }
+            $data['inventory_items'] = array();
+            foreach ($items as $item)
+            {
+                $machine = new Machine($item->serial);
+				$reportdata = new Reportdata($item->serial);
+                $instance['serial'] = $item->serial;
+                $instance['hostname'] = $machine->computer_name;
+                $instance['username'] = $reportdata->console_user;
+                $instance['version'] = $item->version;
+                $instance['bundleid'] = $item->bundleid;
+                $instance['bundlename'] = $item->bundlename;
+                $instance['path'] = $item->path;
+                $data['inventory_items'][] = $instance;
+            }
+            $obj = new View();
+            $obj->view('inventory/inventoryitem_detail', $data);
+        } else {
+            $inventory_item_obj = new InventoryItem();
+            $items = $inventory_item_obj->select(
+                'DISTINCT serial, name, version');
+            $inventory = array();
+            foreach($items as $item)
+            {
+                if(!isset($inventory[$item['name']][$item['version']]))
+                {
+                    $inventory[$item['name']][$item['version']] = 1;
+                } else {
+                    $inventory[$item['name']][$item['version']]++;
+                }
+            }
+            $data['inventory'] = $inventory;
+            $data['page'] = 'inventory_items';
+            $obj = new View();
+            $obj->view('inventory/inventory_items', $data);
+        }
+    }
+
+}
