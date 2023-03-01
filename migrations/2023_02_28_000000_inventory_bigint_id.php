753f7e934834ea85de032e2bff839efcf0f797c8
tuxudo <tuxudo@users.noreply.github.com>
2023-02-28 21:13:28 -0500

    Create 2023_02_28_000000_inventory_bigint_id.php

---
+++ migrations/2023_02_28_000000_inventory_bigint_id.php
@@ -0,0 +1,25 @@
+<?php
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Capsule\Manager as Capsule;
+
+class InventoryBigintId extends Migration
+{
+    private $tableName = 'inventoryitem';
+
+    public function up()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+            $table->bigIncrements('id')->change();
+        });
+    }
+    
+    public function down()
+    {
+        $capsule = new Capsule();
+        $capsule::schema()->table($this->tableName, function (Blueprint $table) {
+            $table->increments('id')->change();
+        });
+    }
+}
